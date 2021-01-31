e6c0a57ceb371e6385df6938f78e3c79346af289
Juliet Yznaga <jay18@duke.edu>
2021-01-31 11:34:17 -0500

    Added boostrap

--- frontend/src/components/login/LoginPage.js
+++ frontend/src/components/login/LoginPage.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.css";
  
 const login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
        <div>
           <h1>Login</h1>

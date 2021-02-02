374eda23da3f29ff75c194c50c2d77b0f4808aa3
Carrie Hunner <carrielhunner@gmail.com>
2021-02-02 13:43:44 -0500

    fixed some small issues in the instrument services

--- frontend/src/components/instrument/InstrumentTablePage.js
+++ frontend/src/components/instrument/InstrumentTablePage.js
@@ -1,12 +1,35 @@
 import React from 'react';
- 
+import InstrumentServices from "../../api/instrumentServices";
+
+const keys = ["vendor", "model number", "serial", "short description", "most recent callibration date"];
+const headers = ["Vendor", "Model", "Serial", "Description", "Last Callibration"];
+const buttonText = ["More"];
+
+let data;
+
 const InstrumentTable = () => {
+    let instrumentServices = new InstrumentServices();
+    data = instrumentServices.getInstruments();
     return (
        <div>
           <h1>Instrument Table</h1>
-           <p>This is where the table with all of our instruments would go</p>
+            {makeTable()}
        </div>
     );
 }
- 
+
+const makeTable = () => {
+    let header = createHeader();
+    let body = createBody();
+
+    return (
+        <Table striped bordered hover>
+            <thead>
+                {header}
+            </thead>
+            {body}
+
+        </Table>)
+}
+
 export default InstrumentTable;

4c80bbba32dc992d37f460b4e4996452f7fbc358
Carrie Hunner <carrielhunner@gmail.com>
2021-02-04 10:19:35 -0500

    switched instrumentPage to a class as opposed to function, this is going to be necessary for calling async functions and keeping track of the popup

--- frontend/src/components/instrument/InstrumentTablePage.js
+++ frontend/src/components/instrument/InstrumentTablePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import InstrumentServices from "../../api/instrumentServices";
 import FilterBar from "./InstrumentFilterBar";
 import logo from '../../assets/HPT_logo_crop.png';
@@ -7,105 +7,129 @@ import './instrument.css';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 
-import { useHistory } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 const keys = ["vendor", "model number", "serial", "short description", "most recent callibration date"];
 const headerTextArr = ["Vendor", "Model", "Serial", "Description", "Last Callibration", "Next Callibration", "More", "Callibration Certificate"];
+const instrumentServices = new InstrumentServices();
 
-let data;
-let history;
+class InstrumentTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            redirect: null,   //this will be a url if a redirect is necessary
+            tableData: []
+        }
 
-const InstrumentTable = () => {
-    let instrumentServices = new InstrumentServices();
-    data = instrumentServices.getInstruments();
-    history = useHistory();
-    return (
-        <div className="background">
-        <div className="row mainContent">
-            <div className="col-2 text-center">
-                <img src={logo} alt="Logo" />
+        //need to bind any event callbacks
+        this.onDetailClicked = this.onDetailClicked.bind(this);
+    }
+    //make async calls here
+    componentDidMount() {
+        let data = instrumentServices.getInstruments();
+        this.setState({
+            tableData: data
+        })
+    }
+
+    render() {
+        if (this.state.redirect !== null) {
+            return (
+                <Redirect to={this.state.redirect} />
+            )
+        }
+        return (
+            <div className="background">
+                <div className="row mainContent">
+                    <div className="col-2 text-center">
+                        <img src={logo} alt="Logo" />
+                    </div>
+                    <div className="col-10">
+                        <h1>Instrument Table</h1>
+                        <FilterBar />
+                        {this.makeTable()}
+                    </div>
+                </div>
             </div>
-            <div className="col-10">
-                <h1>Instrument Table</h1>
-                <FilterBar />
-                {makeTable()}
-            </div>
-        </div>
-        </div>
-    );
-}
+        );
+    }
 
-const makeTable = () => {
-    let header = createHeader();
-    let body = createBody();
 
-    return (
-        <Table striped bordered hover>
-            <thead>
-                {header}
-            </thead>
-            {body}
+    makeTable() {
+        let header = this.createHeader();
+        let body = this.createBody();
 
-        </Table>)
-}
+        return (
+            <Table striped bordered hover>
+                <thead>
+                    {header}
+                </thead>
+                {body}
 
-const createHeader = () => {
-    let header = [];
-    header.push(
-        <th>#</th>
-    )
-    headerTextArr.forEach(h => {
+            </Table>)
+    }
+
+    createHeader() {
+        let header = [];
         header.push(
-            <th>{h}</th>
+            <th>#</th>
         )
-    })
-    return (
-        <tr>
-            {header}
-        </tr>
-    )
-}
-
-const createBody = () => {
-    let rows = [];
-    let count = 1;
-    data.forEach(currentData => {
-        let rowElements = []
-        rowElements.push(
-            <td>{count}</td>
-        )
-        count++;
-        keys.forEach(k => {
-            rowElements.push(
-                <td>{currentData[k]}</td>
+        headerTextArr.forEach(h => {
+            header.push(
+                <th>{h}</th>
             )
         })
-
-        rowElements.push(
-            <td>TBD</td>
-        )
-        rowElements.push(
-            <td><Button value={currentData["instrument pk"]} onClick={onDetailClicked}>More</Button></td>
-        )
-        rowElements.push(
-            <td><Button>Download</Button></td>
-        )
-        let currentRow = (
+        return (
             <tr>
-                {rowElements}
+                {header}
             </tr>
         )
-        rows.push(currentRow);
-    })
-    return (
-        <tbody>
-            {rows}
-        </tbody>
-    );
-}
+    }
 
-const onDetailClicked = (e) => {
-    history.push(`/instruments/${e.target.value}`);
-}
+    createBody() {
+        let rows = [];
+        let count = 1;
+        this.state.tableData.forEach(currentData => {
+            let rowElements = []
+            rowElements.push(
+                <td>{count}</td>
+            )
+            count++;
+            keys.forEach(k => {
+                rowElements.push(
+                    <td>{currentData[k]}</td>
+                )
+            })
 
+            rowElements.push(
+                <td>TBD</td>
+            )
+            rowElements.push(
+                <td><Button value={currentData["instrument pk"]} onClick={this.onDetailClicked}>More</Button></td>
+            )
+            rowElements.push(
+                <td><Button>Download</Button></td>
+            )
+            let currentRow = (
+                <tr>
+                    {rowElements}
+                </tr>
+            )
+            rows.push(currentRow);
+        })
+        return (
+            <tbody>
+                {rows}
+            </tbody>
+        );
+    }
+
+
+    onDetailClicked(e) {
+        //history.push(`/instruments/${e.target.value}`);
+        this.setState({
+            redirect: `/instruments/${e.target.value}`
+        });
+    }
+}
 export default InstrumentTable;

fb7881d2698c0f7c24d5bd5dbb6ad846396545d6
natalie.novitsky <nen4@duke.edu>
2021-02-05 11:39:19 -0500

    cleaning up users

--- backend/tables/models.py
+++ backend/tables/models.py
@@ -1,19 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 one_line = 100
 two_line = 200
-
-
-class User(models.Model):
-    username = models.CharField(max_length=one_line, primary_key=True)
-    email = models.EmailField(max_length=two_line)
-    first_name = models.CharField(max_length=one_line)
-    last_name = models.CharField(max_length=one_line)
-    password = models.CharField(max_length=50) #forms.CharField(widget=forms.PasswordInput)
-    #isAdmin = models.BooleanField()
-
-    def __str__(self):
-        return self.username
 
 
 class ItemModel(models.Model):
--- backend/tables/serializers.py
+++ backend/tables/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('username', 'first_name')
 
 
 class UserSerializerWithToken(serializers.ModelSerializer):

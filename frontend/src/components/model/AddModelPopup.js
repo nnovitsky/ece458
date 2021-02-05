5e900eb9c86c9c148679aabe8c709796bab60e1d
Carrie Hunner <carrielhunner@gmail.com>
2021-02-05 12:50:44 -0500

    made an addmodel popup

---
+++ frontend/src/components/model/AddModelPopup.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+import GenericPopup from '../generic/GenericPopup';
+import FilterField from '../generic/FilterField';
+
+//props
+//'isShown' a boolean if the popup is visible
+//'onSubmit' a handler that will be passed the new instrument information
+//'onClose' a handler for when the popup is closed NOTE: called after a function in this file
+//'getVendorSearchResults' a handler to be called with part of a vendor searched
+
+let newModel = {
+    model: '',
+    vendor: '',
+    description: '',
+    comment: '',
+    callibration: ''
+}
+
+const modelName = "model";
+const vendorName = "vendor";
+const descriptionName = "description";
+const commentName = "comment";
+const callibrationName = "callibration";
+
+const addModelPopup = (props) => {
+    let body = makeBody(props.getVendorSearchResults);
+    return (
+        <GenericPopup
+            show={props.isShown}
+            body={body}
+            headerText="Add Model"
+            closeButtonText="Cancel"
+            submitButtonText="Create Model"
+            onClose={props.onClose}
+            onSubmit={(e) => onSubmit(e, props.onSubmit)}
+
+        />
+    )
+}
+
+const makeBody = (getVendorSearchResults) => {
+    return (
+        <Form className="popup">
+            <Form.Label>Model Number</Form.Label>
+            <Form.Control required type="text" name={modelName} onChange={onTextInput} placeholder="Enter Model Number" />
+
+            <Form.Label>Vendor</Form.Label>
+            <FilterField
+                onTextInput={onTextInput}
+                name={vendorName}
+                dropdownResults={getVendorSearchResults(newModel.vendor)}
+                fieldName="Enter Vendor"
+            />
+            <Form.Label>Description</Form.Label>
+            <Form.Control required type="text" name={descriptionName} onChange={onTextInput} placeholder="Enter Description" />
+
+            <Form.Label>Comments</Form.Label>
+            <Form.Control as="textarea" rows={3} name={commentName} onChange={onTextInput} />
+
+            <Form.Label>Callibration Frequency (days)</Form.Label>
+            <Form.Control required type="text" name={callibrationName} onChange={onTextInput} placeholder="Enter Callibration Frequency" />
+        </Form>
+    )
+}
+
+const onTextInput = (e) => {
+    let val = e.target.value;
+    switch (e.target.name) {
+        case modelName:
+            newModel.model = val
+            return;
+        case vendorName:
+            newModel.vendor = val;
+            return;
+        case descriptionName:
+            newModel.description = val;
+            return;
+        case commentName:
+            newModel.comment = val;
+            return
+        case callibrationName:
+            newModel.callibration = val;
+            return
+        default:
+            return;
+    }
+}
+
+const onSubmit = (e, parentHandler) => {
+    if (isValid) {
+        console.log(newModel)
+        parentHandler(newModel);
+    }
+}
+
+const isValid = () => {
+    return true;
+}
+
+export default addModelPopup;
--- frontend/src/components/model/ModelTablePage.js
+++ frontend/src/components/model/ModelTablePage.js
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import ModelServices from "../../api/modelServices";
 import ModelFilterBar from "./ModelFilterBar";
 import ModelTable from "./ModelTable";
+import AddModelPopup from "./AddModelPopup";
 import { Redirect } from "react-router-dom";
 
 import '../generic/General.css';
@@ -23,46 +24,63 @@ class ModelTablePage extends Component {
                 model: '',
                 vendor: '',
                 description: ''
+            },
+            addModelPopup: {
+                isShown: false,
             }
         }
 
         //binding
         this.onDetailClicked = this.onDetailClicked.bind(this);
         this.onFilteredSearch = this.onFilteredSearch.bind(this);
+        this.onAddModelClosed = this.onAddModelClosed.bind(this);
+        this.onAddModelSubmit = this.onAddModelSubmit.bind(this);
+        this.onGetVendorSearchResults = this.onGetVendorSearchResults.bind(this);
     }
 
     componentDidMount() {
         let data = modelServices.getModels()
         this.setState({
+            redirect: null,
             tableData: data
         })
     }
 
     render() {
-        console.log(this.state.filters)
+        console.log(this.state)
         if (this.state.redirect !== null) {
             return (<Redirect to={this.state.redirect} />)
         }
-    return (
-        <div className="background">
-        <div className="row mainContent">
-            <div className="col-2 text-center">
-                <img src={logo} alt="Logo" />
-            </div>
-                <div className="col-10">
-                <h1>Models</h1>
-                    <ModelFilterBar
-                        onSearch={this.onFilteredSearch}
-                    />
-                    <ModelTable
-                        data={this.state.tableData}
-                        onDetailRequested={this.onDetailClicked}
-                    />
+        return (
+            <div>
+                <AddModelPopup
+                    isShown={this.state.addModelPopup.isShown}
+                    onSubmit={this.onAddModelSubmit}
+                    onClose={this.onAddModelClosed}
+                    getVendorSearchResults={this.onGetVendorSearchResults}
+                />
+
+                <div className="background">
+                    <div className="row mainContent">
+                        <div className="col-2 text-center">
+                            <img src={logo} alt="Logo" />
+                            <Button onClick={this.onAddModelClicked}>Add Model</Button>
+                        </div>
+                        <div className="col-10">
+                            <h1>Models</h1>
+                            <ModelFilterBar
+                                onSearch={this.onFilteredSearch}
+                            />
+                            <ModelTable
+                                data={this.state.tableData}
+                                onDetailRequested={this.onDetailClicked}
+                            />
+                        </div>
+                    </div>
                 </div>
-        </div>
-        </div>
-    );
-}
+            </div>
+        );
+    }
 
 
 
@@ -76,6 +94,34 @@ class ModelTablePage extends Component {
         this.setState({
             ...this.state,
             filters: newFilter
+        })
+    }
+
+    onAddModelSubmit(newModel) {
+        console.log("New model added")
+        console.log(newModel);
+        this.onAddModelClosed();
+    }
+
+    onAddModelClosed() {
+        this.setState({
+            addModelPopup: {
+                ...this.state.addModelPopup,
+                isShown: false
+            }
+        })
+    }
+
+    onGetVendorSearchResults(search) {
+        return [];
+    }
+
+    onAddModelClicked = () => {
+        this.setState({
+            addModelPopup: {
+                ...this.state.addModelPopup,
+                isShown: true
+            }
         })
     }
 }

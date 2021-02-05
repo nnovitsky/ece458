0f02f9757b9ed9c192574e832077a543e3509274
natalie.novitsky <nen4@duke.edu>
2021-02-05 10:00:00 -0500

    adding user auth with jwt

--- backend/tables/views.py
+++ backend/tables/views.py
@@ -2,15 +2,41 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import status
-
+from rest_framework import status, permissions
+from rest_framework.views import APIView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from backend.tables.models import ItemModel
+from django.contrib.auth.models import User
 from backend.tables.serializers import *
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tables index.")
+
+
+@api_view(['GET'])
+def current_user(request):
+    """
+    Determine the current user by their token, and return their data
+    """
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+class UserList(APIView):
+    """
+    Create a new user. It's called 'UserList' because normally we'd have a get
+    method here too, for retrieving a list of all User objects.
+    """
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'POST'])

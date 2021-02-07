fa4f8e1dd982c4a6331a9f750aab5876232a38a9
Juliet Yznaga <jay18@duke.edu>
2021-02-07 15:24:40 -0500

    Merged with main

--- frontend/src/api/authServices.js
+++ frontend/src/api/authServices.js
@@ -9,34 +9,18 @@ export default class AuthServices {
     constructor() { }
 
     async login(data) {
-      return fetch('http://localhost:8000/token-auth/', {
+      return fetch('http://localhost:8000/token_auth/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
       });
-        /*fetch('http://localhost:8000/token-auth/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-          })
-            .then(res => res.json())
-            .then(json => {
-              localStorage.setItem('token', json.token);
-              this.setState({
-                logged_in: true,
-                displayed_form: '',
-                username: json.user.username
-              });
-            });*/
     }
 
     async getCurrentUser(token) {
 
-      return fetch('http://localhost:8000/tables/current_user/', {
+      return fetch('http://localhost:8000/current_user/', {
         headers: {
           Authorization: `JWT ${token}`
         }

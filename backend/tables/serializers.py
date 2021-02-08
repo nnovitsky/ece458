edd723ea0aecf3834ce7ba128a3a7080bc18d769
natalie.novitsky <nen4@duke.edu>
2021-02-08 11:51:05 -0500

    get new token when user details change

--- backend/tables/serializers.py
+++ backend/tables/serializers.py
@@ -38,6 +38,23 @@ class UserSerializerWithToken(serializers.ModelSerializer):
         fields = ('token', 'username', 'password', 'first_name', 'last_name', 'email')
 
 
+class UserEditSerializer(serializers.ModelSerializer):
+
+    token = serializers.SerializerMethodField()
+
+    def get_token(self, obj):
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(obj)
+        token = jwt_encode_handler(payload)
+        return token
+
+    class Meta:
+        model = User
+        fields = ('token', 'username', 'first_name', 'last_name', 'email')
+
+
 class ItemModelSerializer(serializers.ModelSerializer):
 
     class Meta:

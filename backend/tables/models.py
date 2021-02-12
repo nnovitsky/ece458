c7ee0a56b9d9f1bf7db2a62f0da4e9742f1cd367
Jack Wood <jackwood33@gmail.com>
2021-02-12 12:28:09 -0500

    New config file for character_limits + updating models to contain them

--- backend/tables/models.py
+++ backend/tables/models.py
@@ -1,7 +1,10 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
-import datetime
+
+from ..config.character_limits import *
 
 one_line = 100
 two_line = 200
@@ -11,11 +14,12 @@ class ItemModel(models.Model):
     """
     Equipment model with unique vendor + model number pair.
     """
-    vendor = models.CharField(max_length=one_line)
-    model_number = models.CharField(max_length=one_line)
-    description = models.CharField(max_length=two_line)
-    comment = models.CharField(max_length=two_line, blank=True)
-    calibration_frequency = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    vendor = models.CharField(max_length=VENDOR_MAX_LENGTH)
+    model_number = models.CharField(max_length=MODEL_NUM_MAX_LENGTH)
+    description = models.CharField(max_length=DESC_MAX_LENGTH)
+    comment = models.CharField(max_length=COMMENT_MAX_LENGTH, blank=True)
+    calibration_frequency = models.IntegerField(default=0, validators=[MinValueValidator(0)],
+                                                max_length=CALIBRATION_FREQUENCY_MAX_LENGTH)
 
     def __str__(self):
         return self.vendor + " " + self.model_number
@@ -29,8 +33,8 @@ class Instrument(models.Model):
     Instance of a model with unique model + serial number pair.
     """
     item_model = models.ForeignKey(ItemModel, on_delete=models.CASCADE)
-    serial_number = models.CharField(max_length=one_line)
-    comment = models.CharField(max_length=two_line, blank=True)
+    serial_number = models.CharField(max_length=SERIAL_NUM_MAX_LENGTH)
+    comment = models.CharField(max_length=COMMENT_MAX_LENGTH, blank=True)
 
     def __str__(self):
         return str(self.item_model) + " " + self.serial_number
@@ -46,7 +50,7 @@ class CalibrationEvent(models.Model):
     instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
     date = models.DateField(default=datetime.date.today)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    comment = models.CharField(max_length=two_line, blank=True)
+    comment = models.CharField(max_length=COMMENT_MAX_LENGTH, blank=True)
 
     def __str__(self):
         return str(self.instrument) + " " + str(self.date)

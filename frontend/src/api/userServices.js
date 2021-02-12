54c9e885ed5451d512797d81457f29ccb13a37fb
Carrie Hunner <carrielhunner@gmail.com>
2021-02-12 18:07:35 -0500

    apis are protected frm modified/expired tokens

--- frontend/src/api/userServices.js
+++ frontend/src/api/userServices.js
@@ -4,6 +4,7 @@ const API_URL = 'http://localhost:8000';
 export default class UserServices {
     constructor() { }
 
+    // handles modified/expired token
     getUsers() {
         const token = localStorage.getItem('token');
 
@@ -19,25 +20,33 @@ export default class UserServices {
                 Authorization: `JWT ${token}`
             },
         })
-            .then(res => res.json())
-            .then(
-                (json) => {
-                    if (json.detail === 'Signature has expired.') {
-                        console.log("GET NEW TOKEN")
+            .then(res => {
+                if (res.ok) {
+                    return res.json().then(json => {
+                        result.data = json.data;
+                        return result;
+                    });
+                } else {
+                    return res.json().then(json => {
+                        if (json.detail === 'Signature has expired.') {
+                            window.location.reload();
+                            result.success = false;
+                            return result;
+                        }
+                        if (json.detail === 'Error decoding signature.') {
+                            window.location.reload();
+                            result.success = false;
+                            return result;
+                        }
                         result.success = false;
-                    }
-                    result.data = json.data
-                    return result
-                },
-                (error) => {
-                    console.log(error)
-                    result.success = false;
-                    return result
+                        result.errors = json;
+                        return result;
+                    })
                 }
-            )
+            })
     }
 
-
+// handleds expired/modified tokens
     async addUser(username, password, first_name, last_name, email) {
         let data = {
             username: username,
@@ -45,6 +54,11 @@ export default class UserServices {
             first_name: first_name,
             last_name: last_name,
             email: email
+        }
+
+        let result = {
+            success: true,
+            errors: []
         }
         const token = localStorage.getItem('token');
 
@@ -56,18 +70,41 @@ export default class UserServices {
             },
             body: JSON.stringify(data)
         })
-            .then(res => res.json())
-            .then(json => {
-                console.log(json)
+            .then(res => {
+                if (res.ok) {
+                    return result;
+                } else {
+                    return res.json().then(json => {
+                        if (json.detail === 'Signature has expired.') {
+                            window.location.reload();
+                            result.success = false;
+                            return result;
+                        }
+                        if (json.detail === 'Error decoding signature.') {
+                            window.location.reload();
+                            result.success = false;
+                            return result;
+                        }
+                        result.success = false;
+                        result.errors = json;
+                        return result;
+                    })
+                }
             })
     }
 
+    // handles modified/expired tokens
     async editUser(username, password, first_name, last_name) {
         let data = {
             username: username,
             password: password,
             first_name: first_name,
             last_name: last_name,
+        }
+
+        let result = {
+            success: false,
+            errors: []
         }
         const token = localStorage.getItem('token');
 
@@ -79,9 +116,26 @@ export default class UserServices {
             },
             body: JSON.stringify(data)
         })
-            .then(res => res.json())
-            .then(json => {
-                return json;
+            .then(res => {
+                if (res.ok) {
+                    return result;
+                } else {
+                    return res.json().then(json => {
+                        if (json.detail === 'Signature has expired.') {
+                            window.location.reload();
+                            result.success = false;
+                            return result;
+                        }
+                        if (json.detail === 'Error decoding signature.') {
+                            window.location.reload();
+                            result.success = false;
+                            return result;
+                        }
+                        result.success = false;
+                        result.errors = json;
+                        return result;
+                    })
+                }
             })
     }
 

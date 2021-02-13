9515e193bd66b94675bc488bc63c043f0e1e9c50
Carrie Hunner <carrielhunner@gmail.com>
2021-02-13 18:17:25 -0500

    added optional props to the generic popup

--- frontend/src/components/generic/GenericPopup.js
+++ frontend/src/components/generic/GenericPopup.js
@@ -18,6 +18,8 @@ import Alert from 'react-bootstrap/Alert'
 // 'onSubmit': handler for the submit button being clicked
 // 'submitButtonVariant': a string that corresponds to a button variant, eg 'primary' or 'danger'
 // 'errors': an array of strings that are warnings, note an empty array means no errors are displayed, this is an optional field
+// 'isSubmitButtonShown' optional boolean for if the submit button should be shown
+//  'isSecondaryButtonShown': optional boolean for if the secondary button should be shown
 
 const genericPopup = (props) => {
     return (
@@ -34,17 +36,17 @@ const genericPopup = (props) => {
                 <Alert className={"popup-alert"} variant={'danger'} show={props.errors.length > 0}>
                     {makeErrorsParagraphs(props.errors)}
                 </Alert>
-                {buttonArray(props.closeButtonText, props.submitButtonText, props.onClose, props.onSubmit, props.submitButtonVariant)}
+                {buttonArray(props.closeButtonText, props.submitButtonText, props.onClose, props.onSubmit, props.submitButtonVariant, props.isSubmitButtonShown, props.isSecondaryButtonShown)}
 
             </Modal.Footer>
         </Modal>
     )
 }
 
-const buttonArray = (closeText, submitText, onClose, onSubmit, submitButtonVariant) => {
+const buttonArray = (closeText, submitText, onClose, onSubmit, submitButtonVariant, isSubmitButtonShown, isSecondaryButtonShown) => {
     let buttons = [];
 
-    buttons.push(<Button variant="secondary" onClick={onClose}>{closeText}</Button>)
+    buttons.push(<Button variant="secondary" onClick={onClose} hidden={isSecondaryButtonShown}>{closeText}</Button>)
     buttons.push(<Button variant={submitButtonVariant} onClick={onSubmit}>{submitText}</Button>)
 
     let buttonDiv = (
@@ -65,7 +67,9 @@ const makeErrorsParagraphs = (errorsArr) => {
 }
 
 genericPopup.defaultProps = {
-    errors: []
+    errors: [],
+    isSubmitButtonShown: true,
+    isSecondaryButtonShown: true
 }
 
 export default genericPopup;

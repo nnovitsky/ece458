bb38e434c0f3f61642d32cd1dd362b33ef0a1ff5
natalie.novitsky <nen4@duke.edu>
2021-02-13 14:58:25 -0500

    added user/auth tests

--- backend/tables/tests/test_user_views.py
+++ backend/tables/tests/test_user_views.py
@@ -6,30 +6,110 @@ from backend.tables.models import *
 from backend.tables.serializers import *
 
 
-client = Client()
-
-# path('current_user/', views.current_user),
-#     path('token_auth/', obtain_jwt_token),
-#     path('token_refresh/', refresh_jwt_token),
-#     path('create_user/', views.UserCreate.as_view()),
-#     path('api/users/', views.user_list),
 """
 tests: 
 - create user (auth/non auth/anonymous)
 - token auth (auth/non auth)
 - get current user (auth/non auth)
 - edit current user (auth/non auth) (username, fname, lname, pw, email) (check length when limits enforced)
+    - test changing first name successfully
+    - test changing to existing username unsuccessfully
 - token refresh (auth/non auth)
-- user list (auth/non auth)
+- user list (auth)
 """
 
-class CreateUserTest(TestCase):
 
-    def setUp(self):
+class UserTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
         User.objects.all().delete()
-        u = User(username="newUser", password="pw123", first_name="fname", last_name="lname", email="e@g.com")
-        u.is_staff = True
+        cls.userData = {
+            'username': "newUser",
+            'password': "pw123",
+            'first_name': "fname",
+            'last_name': "lname",
+            'email': "e@g.com"
+        }
+        cls.loginData = {
+            'username': cls.userData['username'],
+            'password': cls.userData['password']
+        }
+        cls.token_staff, cls.user_staff = cls.make_user(username="newUser", is_staff=True, data=cls.userData, login=cls.loginData, client=cls.client)
+        cls.token_non_staff, cls.user_non_staff = cls.make_user(username="newUser2", is_staff=False, data=cls.userData, login=cls.loginData, client=cls.client)
+
+    @classmethod
+    def make_user(cls, username, is_staff, data, login, client):
+        data['username'] = username
+        login['username'] = username
+        u = User(**data)
+        u.set_password("pw123")
+        u.is_staff = is_staff
         u.save()
+        response = client.post(reverse('token_auth'), data=json.dumps(login), content_type='application/json')
+        return response.data['token'], u
+
+    def test_create_user_anonymous(self):
+        self.userData['username'] = "newUser3"
+        response = self.client.post(reverse('create_user'), data=json.dumps(self.userData), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_create_user_nonauth(self):
-        response = client.post(reverse())
+        self.userData['username'] = "newUser3"
+        response = self.client.post(reverse('create_user'), data=json.dumps(self.userData),
+                                    content_type='application/json', HTTP_AUTHORIZATION='JWT {}'.format(self.token_non_staff))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_user_auth(self):
+        self.userData['username'] = "newUser3"
+        response = self.client.post(reverse('create_user'), data=json.dumps(self.userData),
+                                    content_type='application/json', HTTP_AUTHORIZATION='JWT {}'.format(self.token_staff))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_auth_user_auth(self):
+        response = self.client.post(reverse('token_auth'), data=json.dumps(self.loginData), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_auth_user_bad_data(self):
+        response = self.client.post(reverse('token_auth'), data=json.dumps({'username': 'bad', 'password': 'none'}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_user_nonauth(self):
+        response = self.client.get(reverse('current_user'))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_user_auth(self):
+        response = self.client.get(reverse('current_user'), HTTP_AUTHORIZATION='JWT {}'.format(self.token_staff))
+        serializer = UserSerializer(self.user_staff)
+        self.assertEqual(serializer.data, response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_list(self):
+        response = self.client.get(reverse('user_list'), {'get_all': 'true'}, HTTP_AUTHORIZATION='JWT {}'.format(self.token_staff))
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        self.assertEqual(serializer.data, response.data['data'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_edit_first_name(self):
+        name = "new name"
+        response = self.client.put(reverse('current_user'), data=json.dumps({'first_name': name}),
+                                   HTTP_AUTHORIZATION='JWT {}'.format(self.token_staff), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['first_name'], name)
+
+    def test_user_edit_bad_username(self):
+        username = self.user_non_staff.username
+        response = self.client.put(reverse('current_user'), data=json.dumps({'username': username}),
+                                   HTTP_AUTHORIZATION='JWT {}'.format(self.token_staff), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_refresh_token_nonauth(self):
+        response = self.client.post(reverse('token_refresh'), data=json.dumps({'token': ""}), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_refresh_token_auth(self):
+        response = self.client.post(reverse('token_refresh'), data=json.dumps({'token': self.token_staff}), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

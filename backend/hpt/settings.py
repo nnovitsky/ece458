02471f77236c0acd4d8fd53c5396aa54a08b4819
Juliet Yznaga <jay18@duke.edu>
2021-02-14 19:30:11 -0500

    Added a comma

--- backend/hpt/settings.py
+++ backend/hpt/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'evolution_one',

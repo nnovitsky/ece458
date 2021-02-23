2b7cf54aa35b11535767b55fde18d664cee86a3b
Jack Wood <jackwood33@gmail.com>
2021-02-22 21:59:46 -0500

    Added and verified model_csv_example end point.

--- backend/hpt/urls.py
+++ backend/hpt/urls.py
@@ -43,5 +43,6 @@ urlpatterns = [
     path('api/import_instruments_csv/', views.import_instruments_csv),
     path('api/export_models_csv/', api_views.ItemModelExport.as_view()),
     path('api/export_instruments_csv/', api_views.InstrumentExport.as_view()),
+    path('api/export_example_model_csv/', views.get_example_model_csv),
 
 ]

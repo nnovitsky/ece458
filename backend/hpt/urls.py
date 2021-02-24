70e30e8a03316008fea742e196a759ca5db13532
natalie.novitsky <nen4@duke.edu>
2021-02-24 11:48:33 -0500

    change to 3000 redirect url

--- backend/hpt/urls.py
+++ backend/hpt/urls.py
@@ -45,5 +45,5 @@ urlpatterns = [
     path('api/export_instruments_csv/', api_views.InstrumentExport.as_view()),
     path('api/export_example_model_csv/', views.get_example_model_csv),
     path('api/export_example_instrument_csv/', views.get_example_instrument_csv),
-    path('oauth/consume/', views.OauthConsume.as_view()),
+    path('api/oauth/consume/', views.OauthConsume.as_view()),
 ]

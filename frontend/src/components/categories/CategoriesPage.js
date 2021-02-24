eb508ce80cf3724b74b8c94860ac4604ca2270a7
Carrie Hunner <carrielhunner@gmail.com>
2021-02-24 17:59:53 -0500

    have a rename popup displaying

--- frontend/src/components/categories/CategoriesPage.js
+++ frontend/src/components/categories/CategoriesPage.js
@@ -4,12 +4,21 @@ import Tab from 'react-bootstrap/Tab';
 import '../generic/General.css';
 import logo from '../../assets/HPT_logo_crop.png';
 import CategoriesTable from './ModelCategoriesTable';
+import RenamePopup from './RenamePopup';
 
 class ModelTablePage extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            renamePopup: {
+                pk: null,
+                currentName: '',
+                newName: '',
+                isShown: false,
+                errors: []
+            },
+            currentTab: 'model',
             modelCategories: {
                 data: [],
                 pagination: {
@@ -19,7 +28,7 @@ class ModelTablePage extends Component {
                     currentPageNum: 1,
                     desiredPage: 1,
                     showAll: false,
-                }
+                },
             },
             instrumentCategories: {
                 data: [],
@@ -31,14 +40,16 @@ class ModelTablePage extends Component {
                     desiredPage: 1,
                     showAll: false,
                 }
-            }
-
+            },
+            
         }
 
         this.onModelTableChange = this.onModelTableChange.bind(this);
-        this.onEditModelClicked = this.onEditModelClicked.bind(this);
+        this.onEditClicked = this.onEditClicked.bind(this);
+        this.onEditClose = this.onEditClose.bind(this);
+        this.onEditSubmit = this.onEditSubmit.bind(this);
         this.onInstrumentTableChange = this.onInstrumentTableChange.bind(this);
-        this.onEditInstrumentClicked = this.onEditInstrumentClicked.bind(this);
+        this.onTabChange = this.onTabChange.bind(this);
     }
 
     async componentDidMount() {
@@ -47,28 +58,30 @@ class ModelTablePage extends Component {
     }
 
     render() {
+        let renamePopup = this.makeRenamePopup();    //will return null if not being displayed
         return (
             <div className="background">
+                {renamePopup}
                 <div className="row mainContent">
                     <div className="col-2 text-center button-col">
                         <img src={logo} alt="Logo" />
                     </div>
                     <div className="col-10">
-                        <Tabs defaultActiveKey="modelCategory">
-                            <Tab eventKey="modelCategory" title="Model Categories">
+                        <Tabs defaultActiveKey={this.state.currentTab} onSelect={this.onTabChange}>
+                            <Tab eventKey="model" title="Model Categories">
                                 <CategoriesTable
                                     data={this.state.modelCategories.data}
                                     onTableChange={this.onModelTableChange}
                                     pagination={{ page: this.state.modelCategories.pagination.currentPageNum, sizePerPage: (this.state.modelCategories.pagination.showAll ? this.state.modelCategories.pagination.resultCount : this.state.modelCategories.pagination.resultsPerPage), totalSize: this.state.modelCategories.pagination.resultCount }}
-                                    onCategoryEdit={this.onEditModelClicked}
+                                    onCategoryEdit={this.onEditClicked}
                                 />
                             </Tab>
-                            <Tab eventKey="instrumentCategory" title="Instrument Categories">
+                            <Tab eventKey="instrument" title="Instrument Categories">
                                 <CategoriesTable
                                     data={this.state.instrumentCategories.data}
                                     onTableChange={this.onInstrumentTableChange}
                                     pagination={{ page: this.state.instrumentCategories.pagination.currentPageNum, sizePerPage: (this.state.instrumentCategories.pagination.showAll ? this.state.instrumentCategories.pagination.resultCount : this.state.instrumentCategories.pagination.resultsPerPage), totalSize: this.state.instrumentCategories.pagination.resultCount }}
-                                    onCategoryEdit={this.onEditInstrumentClicked}
+                                    onCategoryEdit={this.onEditClicked}
                                 />
                             </Tab>
 
@@ -78,6 +91,22 @@ class ModelTablePage extends Component {
                 </div>
             </div>
         )
+    }
+
+    makeRenamePopup() {
+        if (this.state.renamePopup.isShown) {
+            return (
+                <RenamePopup
+                    isShown={this.state.renamePopup.isShown}
+                    onClose={this.onEditClose}
+                    onSubmit={this.onEditSubmit}
+                    errors={this.state.renamePopup.errors}
+                    currentName={this.state.renamePopup.currentName}
+                />
+            )
+        } else {
+            return null;
+        }
     }
 
     async updateModelCategories() {
@@ -120,20 +149,46 @@ class ModelTablePage extends Component {
         })
     }
 
+    onTabChange(e) {
+        this.setState({
+            currentTab: e
+        })
+    }
+
     onModelTableChange(type, { page, sizePerPage }) {
         console.log(type);
     }
 
-    onEditModelClicked(e) {
+    onEditClicked(e) {
         console.log(`Rename requested for ${e.target.name} with pk ${e.target.value}`);
+        this.setState({
+            renamePopup: {
+                ...this.state.renamePopup,
+                pk: e.target.value,
+                currentName: e.target.name,
+                isShown: true,
+            }
+        })
+    }
+
+    onEditClose() {
+        this.setState({
+            renamePopup: {
+                ...this.state.renamePopup,
+                pk: null,
+                currentName: '',
+                isShown: false,
+            }
+        })
+    }
+
+    onEditSubmit(newName) {
+        console.log(`New name: ${newName}`);
+        this.onEditClose();
     }
 
     onInstrumentTableChange(type, { page, sizePerPage }) {
         console.log(type);
-    }
-
-    onEditInstrumentClicked(e) {
-        console.log(`Rename requested for ${e.target.name} with pk ${e.target.value}`);
     }
 
 

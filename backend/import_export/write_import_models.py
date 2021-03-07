435603d08bc2010b0d8c8c3307b92a0d7835f08c
Jack Wood <jackwood33@gmail.com>
2021-03-07 11:17:21 -0500

    Refactoring based on serializer consistencies

--- backend/import_export/write_import_models.py
+++ backend/import_export/write_import_models.py
@@ -2,17 +2,11 @@ import csv
 import io
 
 from backend.tables.serializers import ItemModelSerializer, ItemModelNoCategoriesSerializer
-from backend.tables.models import ItemModelCategory
+from backend.tables.models import ItemModelCategory, CalibrationMode
 from backend.import_export.field_validators import is_blank_row
 
-# TODO: handle load bank cell after "calibration-mode" field decided upon for ItemModels
-# model_keys = ['vendor', 'model_number', 'description', 'comment', 'itemmodelcategory_set',
-#               'load_bank_support', 'calibration_frequency']
-
-model_keys = ['vendor', 'model_number', 'description', 'comment', 'calibration_frequency']
-keys_no_category = ['vendor', 'model_number', 'description', 'comment', 'calibration_frequency']
-keys_category = ['vendor', 'model_number', 'description', 'comment', 'itemmodelcategory_set',
-                 'calibration_frequency']
+model_keys = ['vendor', 'model_number', 'description', 'comment', 'calibration_frequency',
+              'itemmodelcategory_set', 'calibrationmode_set']
 
 VENDOR_INDEX = 0
 MODEL_NUM_INDEX = 1
@@ -22,41 +16,40 @@ MODEL_CATEGORIES_INDEX = 4
 LOAD_BANK_INDEX = 5
 CAL_FREQUENCY_INDEX = 6
 
+load_bank_pk = CalibrationMode.objects.get(name='load_bank').pk
 
-def get_no_category_record(row):
+
+def get_cal_fields(row):
+
     if row[CAL_FREQUENCY_INDEX] == 'N/A':
         cal_freq = 0
     else:
         cal_freq = int(row[CAL_FREQUENCY_INDEX])
 
-    model_info = [row[VENDOR_INDEX], row[MODEL_NUM_INDEX], row[DESC_INDEX],
-                  row[COMMENT_INDEX], cal_freq]
-    model_record = dict(zip(keys_no_category, model_info))
-
-    return model_record
-
-
-def get_category_record(row):
-    if row[CAL_FREQUENCY_INDEX] == 'N/A':
-        cal_freq = 0
+    if len(row[LOAD_BANK_INDEX]) == 0:
+        cal_mode = []
+    elif row[LOAD_BANK_INDEX].strip() == 'Y':
+        cal_mode = [load_bank_pk]
     else:
-        cal_freq = int(row[CAL_FREQUENCY_INDEX])
+        print("Uncaught exception for calibration mode validation")
 
-    model_category_set = set()
-    for category in row[MODEL_CATEGORIES_INDEX].strip().split(' '):
-        category_pk = ItemModelCategory.objects.filter(name=category)[0].pk
-        model_category_set.add(category_pk)
+    return cal_freq, cal_mode
 
-    model_info = [row[VENDOR_INDEX], row[MODEL_NUM_INDEX], row[DESC_INDEX],
-                  row[COMMENT_INDEX], model_category_set, cal_freq]
-    model_record = dict(zip(keys_category, model_info))
 
-    return model_record
+def get_model_category_set(row):
+    model_category_set = []
+    row_categories = row[MODEL_CATEGORIES_INDEX].strip()
+
+    if len(row_categories) > 0:
+        for category in row_categories.split(' '):
+            category_pk = ItemModelCategory.objects.filter(name=category)[0].pk
+            model_category_set.append(category_pk)
+
+    return model_category_set
 
 
 def get_model_list(file):
-    category_models = []
-    no_category_models = []
+    model_records = []
 
     file.seek(0)
     reader = csv.reader(io.StringIO(file.read().decode('utf-8')))
@@ -65,37 +58,25 @@ def get_model_list(file):
         if is_blank_row(row):
             continue
 
-        if len(row[MODEL_CATEGORIES_INDEX].strip()) == 0:
-            no_category_models.append(get_no_category_record(row))
-        else:
-            category_models.append(get_category_record(row))
+        cal_freq, cal_mode = get_cal_fields(row)
+        model_category_set = get_model_category_set(row)
 
-    return no_category_models, category_models
+        model_info = [row[VENDOR_INDEX], row[MODEL_NUM_INDEX], row[DESC_INDEX],
+                      row[COMMENT_INDEX], cal_freq, model_category_set, cal_mode]
+        model_records.append(dict(zip(model_keys, model_info)))
+
+    return model_records
 
 
 def handler(verified_file):
-    db_cat_upload = ''
-    db_no_cat_upload = ''
 
-    no_category_models, category_models = get_model_list(verified_file)
-    if len(no_category_models) > 0:
-        db_no_cat_upload = ItemModelNoCategoriesSerializer(data=no_category_models, many=True)
-        if not db_no_cat_upload.is_valid():
-            return False, None, "Error writing non-category models to db."
+    model_data = get_model_list(verified_file)
+    db_model_upload = ItemModelSerializer(data=model_data, many=True)
 
-    print("non-category upload was valid!")
-    if len(category_models) > 0:
-        db_cat_upload = ItemModelSerializer(data=category_models, many=True)
-        print("db_cat_upload: ", db_cat_upload)
-        if not db_cat_upload.is_valid():
-            print("db_category_upload_response: ", str(db_cat_upload.is_valid()))
-            return False, None, "Error writing category models to db."
+    if not db_model_upload.is_valid():
+        return False, None, f"Error writing models to db: {db_model_upload.errors}"
 
-    upload_list = []
+    upload_results = db_model_upload.save()
 
-    if db_no_cat_upload: upload_list += db_no_cat_upload.save()
-    if db_cat_upload: upload_list += db_cat_upload.save()
-
-    upload_summary = f"Successfully wrote {len(no_category_models) + len(category_models)} model(s) to the database."
-    print("upload_list: ", upload_list)
-    return True, upload_list, upload_summary
+    upload_summary = f"Successfully wrote {len(model_data)} model(s) to the database."
+    return True, upload_results, upload_summary

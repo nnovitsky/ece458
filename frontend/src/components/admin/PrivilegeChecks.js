abac3d979870c57599a419b505ab71f911585c83
Juliet Yznaga <jay18@duke.edu>
2021-03-16 22:36:52 -0400

    Checks are one group

--- frontend/src/components/admin/PrivilegeChecks.js
+++ frontend/src/components/admin/PrivilegeChecks.js
@@ -15,10 +15,8 @@ const privilegeChecks = (props) => {
                 <Form.Group className="form-inline">
                     <Form.Check className="col-sm" id={'admin'+ props.pk} name="admin" label="Admin" type="checkbox" onChange={(privilege) => { props.onChange(mapObjectToArray(privilege, props.pk, props.groups)) }} checked={adminCheck}></Form.Check>
                     <Form.Check className="col-sm" id={'calibrations'+ props.pk} name="calibrations" disabled={adminCheck} label="Calibration" type="checkbox" onChange={(privilege) => { props.onChange(mapObjectToArray(privilege, props.pk, props.groups)) }} checked={caliCheck}></Form.Check>
-                </Form.Group>
-                <Form.Group className="form-inline">
                     <Form.Check className="col-sm" id={'models'+ props.pk} name="models" disabled={adminCheck} label="Model" type="checkbox" onChange={(privilege) => { props.onChange(mapObjectToArray(privilege, props.pk, props.groups)) }} checked={modelCheck}></Form.Check>
-                    <Form.Check className="col-sm" id={'instruments'+ props.pk} name="instruments" disabled={adminCheck || modelCheck} label="Instrument" type="checkbox" onChange={(privilege) => { props.onChange(mapObjectToArray(privilege, props.pk, props.groups)) }} checked={instrumentCheck}></Form.Check>
+                <Form.Check className="col-sm" id={'instruments'+ props.pk} name="instruments" disabled={adminCheck || modelCheck} label="Instrument" type="checkbox" onChange={(privilege) => { props.onChange(mapObjectToArray(privilege, props.pk, props.groups)) }} checked={instrumentCheck}></Form.Check>
                 </Form.Group>
             </Form>
         </div>

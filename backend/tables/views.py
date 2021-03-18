8043a7cee0b1e1231927135ebd7f86d1f8ac57ce
natalie.novitsky <nen4@duke.edu>
2021-03-18 14:52:15 -0400

    block de-admining super admin

--- backend/tables/views.py
+++ backend/tables/views.py
@@ -484,6 +484,9 @@ def toggle_groups(request, user_pk):
         return Response({"description": ["User does not exist."]}, status=status.HTTP_404_NOT_FOUND)
 
     groups = set(request.data['groups'])
+    if other_user.username == ADMIN_USERNAME and "admin" not in groups:
+        return Response({"description": ["Cannot remove permissions of superadmin."]}, status=status.HTTP_400_BAD_REQUEST)
+
     groups = edit_user_groups(groups, other_user)
     if groups == 'ERROR':
         return Response({"description": ["Invalid group name."]}, status=status.HTTP_400_BAD_REQUEST)

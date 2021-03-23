d27303dedf3f4c8f4335f3a0bbeef163b1e191e7
Juliet Yznaga <jay18@duke.edu>
2021-03-23 17:38:32 -0400

    Changed redirect uri

--- backend/tables/oauth.py
+++ backend/tables/oauth.py
@@ -66,7 +66,7 @@ def get_token(code):
     if "OAUTH_REDIRECT_URI" in os.environ:
         redirect_uri = os.environ["OAUTH_REDIRECT_URI"]
     else:
-        redirect_uri = "https://vcm-18868.vm.duke.edu/oauth/consume"
+        redirect_uri = "https://hptmanager.colab.duke.edu/oauth/consume"
 
     payload = urllib.parse.urlencode({
         'grant_type': "authorization_code",

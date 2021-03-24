c4cd8c0d8d790734e863c95c982418db90c4337c
Juliet Yznaga <jay18@duke.edu>
2021-03-24 15:24:15 -0400

    CHanged import privileges

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -3,20 +3,25 @@ import PropTypes from 'prop-types';
 import "../App.css"
 import { NavLink } from 'react-router-dom';
 import profileIcon from '../assets/Profile.png';
+import { hasAdminAccess, hasModelEditAccess, hasInstrumentEditAccess } from './generic/Util.js';
 
 
 function Navigation(props) {
+
+    const isAdmin = hasAdminAccess(props.permissions);
+    const isModelEditor = hasModelEditAccess(props.permissions);
+    const isInstrumentEditor = hasInstrumentEditAccess(props.permissions);
 
     const logged_in_nav = (
         <nav>
             <ul>
                 <li><img style={{height: "20px"}}src={profileIcon} alt="Logo" /></li>
                 <li>{props.user}   </li>
-                <li><NavLink to="/user-profile">   Profile&nbsp;</NavLink></li>
+                <li><NavLink to="/user-profile">Profile&nbsp;</NavLink></li>
                 <li><NavLink to="/models">Models&nbsp;</NavLink></li>
                 <li><NavLink to="/instruments">Instruments&nbsp;</NavLink></li>
-                {props.is_admin ? <li><NavLink to="/import">Import&nbsp;</NavLink></li> : null}
-                {props.is_admin ? <li><NavLink to="/admin">Admin&nbsp;</NavLink></li> : null}
+                {(isAdmin || isModelEditor || isInstrumentEditor) ? <li><NavLink to="/import">Import&nbsp;</NavLink></li> : null}
+                {isAdmin ? <li><NavLink to="/admin">Admin&nbsp;</NavLink></li> : null}
                 <li className="col order-last" style={{ textAlign: "right" }}><NavLink onClick={props.handle_logout} to="/">Logout&nbsp;</NavLink></li>
             </ul>
         </nav>
@@ -42,6 +47,5 @@ export default Navigation;
 Navigation.propTypes = {
     logged_in: PropTypes.bool.isRequired,
     handle_logout: PropTypes.func.isRequired,
-    is_admin: PropTypes.bool.isRequired,
     user: PropTypes.string
 }

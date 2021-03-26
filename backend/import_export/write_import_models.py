d96ad057148816d2e3db6c252b119da21f7ab4d1
Jack Wood <jackwood33@gmail.com>
2021-03-26 19:35:14 -0400

    support for klufe models in model imports

--- backend/import_export/write_import_models.py
+++ backend/import_export/write_import_models.py
@@ -15,8 +15,19 @@ MODEL_NUM_INDEX = 1
 DESC_INDEX = 2
 COMMENT_INDEX = 3
 MODEL_CATEGORIES_INDEX = 4
-LOAD_BANK_INDEX = 5
+CALIBRATION_SUPPORT_INDEX = 5
 CAL_FREQUENCY_INDEX = 6
+
+
+def get_klufe_pk():
+    try:
+        klufe_pk = CalibrationMode.objects.get(name='klufe_k5700').pk
+    except CalibrationMode.DoesNotExist:
+        cal_mode = CalibrationMode(name='klufe_k5700')
+        cal_mode.save()
+        klufe_pk = cal_mode.pk
+
+    return klufe_pk
 
 
 def get_load_bank_pk():
@@ -30,17 +41,19 @@ def get_load_bank_pk():
     return load_bank_pk
 
 
-def get_cal_fields(row, load_bank_pk):
+def get_cal_fields(row, load_bank_pk, klufe_pk):
 
     if row[CAL_FREQUENCY_INDEX] == 'N/A':
         cal_freq = 0
     else:
         cal_freq = int(row[CAL_FREQUENCY_INDEX])
 
-    if len(row[LOAD_BANK_INDEX]) == 0:
+    if len(row[CALIBRATION_SUPPORT_INDEX]) == 0:
         cal_mode = []
-    elif row[LOAD_BANK_INDEX].strip() == 'Y':
+    elif row[CALIBRATION_SUPPORT_INDEX].strip() == 'Load-Bank':
         cal_mode = [load_bank_pk]
+    elif row[CALIBRATION_SUPPORT_INDEX].strip() == 'Klufe':
+        cal_mode = [klufe_pk]
     else:
         print("Uncaught exception for calibration mode validation")
 
@@ -64,7 +77,7 @@ def get_model_category_set(row, db_categories):
     return model_category_set
 
 
-def get_model_list(file, load_bank_pk, db_categories):
+def get_model_list(file, load_bank_pk, klufe_pk, db_categories):
     model_records = []
 
     file.seek(0)
@@ -74,7 +87,7 @@ def get_model_list(file, load_bank_pk, db_categories):
         if is_blank_row(row):
             continue
 
-        cal_freq, cal_mode = get_cal_fields(row, load_bank_pk)
+        cal_freq, cal_mode = get_cal_fields(row, load_bank_pk, klufe_pk)
         model_category_set = get_model_category_set(row, db_categories)
 
         model_info = [row[VENDOR_INDEX], row[MODEL_NUM_INDEX], row[DESC_INDEX],
@@ -86,11 +99,12 @@ def get_model_list(file, load_bank_pk, db_categories):
 
 def handler(verified_file):
     load_bank_pk = get_load_bank_pk()
+    klufe_pk = get_klufe_pk()
     db_categories = list(ItemModelCategory.objects.values_list('name', flat=True))
 
     try:
         with transaction.atomic():
-            model_data = get_model_list(verified_file, load_bank_pk, db_categories)
+            model_data = get_model_list(verified_file, load_bank_pk, klufe_pk, db_categories)
             db_model_upload = ItemModelSerializer(data=model_data, many=True)
             if db_model_upload.is_valid(): upload_results = db_model_upload.save()
     except IOError:

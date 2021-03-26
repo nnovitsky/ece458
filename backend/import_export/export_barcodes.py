142fa60e9316111ee57ab9d26117ef7589dd0e78
Jack Wood <jackwood33@gmail.com>
2021-03-26 12:34:37 -0400

    Adding buffer + BC libraries

--- backend/import_export/export_barcodes.py
+++ backend/import_export/export_barcodes.py
@@ -6,6 +6,14 @@ from rest_framework.response import Response
 from django.http import FileResponse
 from backend.tables.serializers import ListInstrumentReadSerializer
 from backend.tables.models import Instrument
+from reportlab.graphics.barcode import code128
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+
+
+def make_pdf(buffer, asset_tags, file_name):
+    
+
 
 
 def filter_instrument_pks(raw_pks):
@@ -29,5 +37,9 @@ def handler(instrument_pks, select_all):
         filtered_pks = filter_instrument_pks(instrument_pks)
         asset_tags = get_asset_tags(filtered_pks)
 
+    buffer = make_pdf(BytesIO(), asset_tags)
+    buffer.seek(0)
+
     filename = f"{date.today().strftime('%Y_%m_%d')}_barcodes.pdf"
-    return False
+
+    return FileResponse(buffer, as_attachment=True, filename=filename)

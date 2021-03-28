b0d9298288c0f08b880772f973efffd733da656b
Jack Wood <jackwood33@gmail.com>
2021-03-28 19:37:52 -0400

    subbing global var for local as potential bug fix to pdf

--- backend/import_export/export_pdf.py
+++ backend/import_export/export_pdf.py
@@ -54,7 +54,6 @@ FILE_TYPE_INDEX = 0
 FILE_NAME_INDEX = 1
 styles = getSampleStyleSheet()
 elements = []
-pdf_merge = False
 
 
 def get_fields(instrument):
@@ -339,6 +338,8 @@ def fill_pdf(buffer, fields, cal_file_data, cal_pk):
     if cal_file_data[FILE_TYPE_INDEX] == 'Artifact' and cal_file_data[FILE_NAME_INDEX].split('.')[-1].lower() == 'pdf':
         global pdf_merge
         pdf_merge = True
+    else:
+        pdf_merge = False
 
     if cal_file_data[FILE_TYPE_INDEX] == 'Artifact' and cal_file_data[FILE_NAME_INDEX].split('.')[-1].lower() == 'xlsx':
         elements.append(get_xlsx_hyperlink(cal_pk))
@@ -350,7 +351,7 @@ def fill_pdf(buffer, fields, cal_file_data, cal_pk):
         get_klufe_table(cal_pk)
 
     doc.build(elements)
-    return buffer
+    return buffer, pdf_merge
 
 
 def merge_pdf(cal_pk, buffer):
@@ -366,12 +367,11 @@ def merge_pdf(cal_pk, buffer):
 
 
 def handler(instrument):
-    global pdf_merge
 
     certificate_info, cal_file_data, cal_pk = get_fields(instrument)
     instrument_name = str(instrument).replace(" ", "_")
     filename = f"{instrument_name}_calibration_record_{date.today().strftime('%Y_%m_%d')}.pdf"
-    buffer = fill_pdf(BytesIO(), certificate_info, cal_file_data, cal_pk)
+    buffer, pdf_merge = fill_pdf(BytesIO(), certificate_info, cal_file_data, cal_pk)
     buffer.seek(0)
 
     if pdf_merge:

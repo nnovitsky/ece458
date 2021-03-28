aa9a345f55f4b6ea1c7f9680b042ad370ce82e8a
Jack Wood <jackwood33@gmail.com>
2021-03-27 23:50:35 -0400

    Adding functionality to export_pdf.py module + spacing typo in klufe_config.py

--- backend/config/klufe_config.py
+++ backend/config/klufe_config.py
@@ -8,7 +8,7 @@ VOLTAGE_LEVELS = [
     {
         'source_voltage': 3.500,
         'is_AC': False,
-        'description': "3.500V " + u"\u00B1" + "0.001",
+        'description': "3.500V " + u"\u00B1" + " 0.001",
         'lower': 3.500-0.001,
         'upper': 3.500+0.001,
         'index':0
@@ -17,7 +17,7 @@ VOLTAGE_LEVELS = [
         'source_voltage': 3.513,
         'is_AC': True,
         'Hz': 50.0,
-        'description': "3.500V " + u"\u00B1" + "0.002",
+        'description': "3.500V " + u"\u00B1" + " 0.002",
         'lower': 3.500-0.002,
         'upper': 3.500+0.002,
         'index':1
@@ -26,7 +26,7 @@ VOLTAGE_LEVELS = [
         'source_voltage': 100.0,
         'is_AC': True,
         'Hz': 20000.0,
-        'description': "100.0V " + u"\u00B1" + "0.2",
+        'description': "100.0V " + u"\u00B1" + " 0.2",
         'lower': 100-0.2,
         'upper': 100+0.2,
         'index':2
@@ -35,7 +35,7 @@ VOLTAGE_LEVELS = [
         'source_voltage': 3.500,
         'is_AC': True,
         'Hz': 10000.0,
-        'description': "3.500V " + u"\u00B1" + "0.004",
+        'description': "3.500V " + u"\u00B1" + " 0.004",
         'lower': 3.500-0.004,
         'upper': 3.500+0.004,
         'index':3
@@ -44,7 +44,7 @@ VOLTAGE_LEVELS = [
         'source_voltage': 35.00,
         'is_AC': True,
         'Hz': 10000.0,
-        'description': "35.00V " + u"\u00B1" + "0.04",
+        'description': "35.00V " + u"\u00B1" + " 0.04",
         'lower': 35.00-0.04,
         'upper': 35.00+0.04,
         'index':4

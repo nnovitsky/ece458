26bbe916d805068a25b63e8302bbc3491b7ff114
root <root@vcm-18278.vm.duke.edu>
2021-04-17 23:51:54 -0400

    Added oauth stuff

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://hptmanager-dev.colab.duke.edu';
+const API_URL = 'https://hptmanager.colab.duke.edu';
 //const API_URL = 'https://vcm-18868.vm.duke.edu';
 //const API_URL = 'http://localhost:8000';
 

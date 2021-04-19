c50e99a5fcf97f39b95573bf6f60e56760e693cb
Juliet Yznaga <julietyznaga@gmail.com>
2021-04-19 17:39:19 -0400

    Added more tweeks

--- frontend/src/components/formCal/formGroups/TextInputGroup.js
+++ frontend/src/components/formCal/formGroups/TextInputGroup.js
@@ -7,7 +7,7 @@ const textInputGroup = (props) => {
     return (
         <Form.Group style={{overflow: 'auto'}}>
             <Form.Label className="required-field">{props.label}</Form.Label>
-            <Form.Control id={props.id} value={props.value} onChange={props.onChange}></Form.Control>
+            <Form.Control id={props.id} value={props.value} onChange={props.onChange} style={{width: "90%", marginLeft: "5px"}}></Form.Control>
             <Form.Label className="subtext">{text}</Form.Label>
         </Form.Group>
     )
